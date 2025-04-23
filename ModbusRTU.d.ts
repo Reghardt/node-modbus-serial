85a80736771bba79a83a9b0c275ca2071bc4d5ba
Reghardt <reghardt1000@gmail.com>
2025-04-23 18:07:02 +0200

    before/after send callbacks

--- ModbusRTU.d.ts
+++ ModbusRTU.d.ts
@@ -1,27 +1,73 @@
-import { Socket, SocketConstructorOpts, TcpSocketConnectOpts } from 'net';
+import { Socket, SocketConstructorOpts, TcpSocketConnectOpts } from "net";
 import { TestPort } from "./TestPort";
-import { PortInfo } from '@serialport/bindings-cpp';
+import { PortInfo } from "@serialport/bindings-cpp";
 
 export class ModbusRTU {
   constructor(port?: any);
-  static TestPort: typeof TestPort
+  static TestPort: typeof TestPort;
 
-  static getPorts(): Promise<PortInfo[]>
+  static getPorts(): Promise<PortInfo[]>;
 
   open(callback?: Function): void;
   close(callback?: Function): void;
   destroy(callback?: Function): void;
 
-  writeFC1(address: number, dataAddress: number, length: number, next: NodeStyleCallback<ReadCoilResult>): void;
-  writeFC2(address: number, dataAddress: number, length: number, next: NodeStyleCallback<ReadCoilResult>): void;
-  writeFC3(address: number, dataAddress: number, length: number, next: NodeStyleCallback<ReadRegisterResult>): void;
-  writeFC4(address: number, dataAddress: number, length: number, next: NodeStyleCallback<ReadRegisterResult>): void;
-  writeFC5(address: number, dataAddress: number, state: boolean, next: NodeStyleCallback<WriteCoilResult>): void;
-  writeFC6(address: number, dataAddress: number, value: number, next: NodeStyleCallback<WriteRegisterResult>): void;
+  writeFC1(
+    address: number,
+    dataAddress: number,
+    length: number,
+    next: NodeStyleCallback<ReadCoilResult>
+  ): void;
+  writeFC2(
+    address: number,
+    dataAddress: number,
+    length: number,
+    next: NodeStyleCallback<ReadCoilResult>
+  ): void;
+  writeFC3(
+    address: number,
+    dataAddress: number,
+    length: number,
+    next: NodeStyleCallback<ReadRegisterResult>
+  ): void;
+  writeFC4(
+    address: number,
+    dataAddress: number,
+    length: number,
+    next: NodeStyleCallback<ReadRegisterResult>
+  ): void;
+  writeFC5(
+    address: number,
+    dataAddress: number,
+    state: boolean,
+    next: NodeStyleCallback<WriteCoilResult>
+  ): void;
+  writeFC6(
+    address: number,
+    dataAddress: number,
+    value: number,
+    next: NodeStyleCallback<WriteRegisterResult>
+  ): void;
 
-  writeFC15(address: number, dataAddress: number, states: Array<boolean>, next: NodeStyleCallback<WriteMultipleResult>): void;
-  writeFC16(address: number, dataAddress: number, values: Array<number>, next: NodeStyleCallback<WriteMultipleResult>): void;
-  writeFC22(address: number, dataAddress: number, andMask: number, orMask: number, next: NodeStyleCallback<WriteMaskRegisterResult>): void;
+  writeFC15(
+    address: number,
+    dataAddress: number,
+    states: Array<boolean>,
+    next: NodeStyleCallback<WriteMultipleResult>
+  ): void;
+  writeFC16(
+    address: number,
+    dataAddress: number,
+    values: Array<number>,
+    next: NodeStyleCallback<WriteMultipleResult>
+  ): void;
+  writeFC22(
+    address: number,
+    dataAddress: number,
+    andMask: number,
+    orMask: number,
+    next: NodeStyleCallback<WriteMaskRegisterResult>
+  ): void;
 
   // Connection shorthand API
   connectRTU(path: string, options: SerialPortOptions, next: Function): void;
@@ -30,24 +76,49 @@ export class ModbusRTU {
   connectTCP(ip: string, options: TcpPortOptions): Promise<void>;
   connectUDP(ip: string, options: UdpPortOptions, next: Function): void;
   connectUDP(ip: string, options: UdpPortOptions): Promise<void>;
-  connectTcpRTUBuffered(ip: string, options: TcpRTUPortOptions, next: Function): void;
+  connectTcpRTUBuffered(
+    ip: string,
+    options: TcpRTUPortOptions,
+    next: Function
+  ): void;
   connectTcpRTUBuffered(ip: string, options: TcpRTUPortOptions): Promise<void>;
   connectTelnet(ip: string, options: TelnetPortOptions, next: Function): void;
   connectTelnet(ip: string, options: TelnetPortOptions): Promise<void>;
   connectC701(ip: string, options: C701PortOptions, next: Function): void;
   connectC701(ip: string, options: C701PortOptions): Promise<void>;
-  connectRTUBuffered(path: string, options: SerialPortOptions, next: Function): void;
+  connectRTUBuffered(
+    path: string,
+    options: SerialPortOptions,
+    next: Function
+  ): void;
   connectRTUBuffered(path: string, options: SerialPortOptions): Promise<void>;
-  connectAsciiSerial(path: string, options: SerialPortOptions, next: Function): void;
+  connectAsciiSerial(
+    path: string,
+    options: SerialPortOptions,
+    next: Function
+  ): void;
   connectAsciiSerial(path: string, options: SerialPortOptions): Promise<void>;
   linkTCP(socket: Socket, options: TcpPortOptions, next: Function): void;
   linkTCP(socket: Socket, options: TcpPortOptions): Promise<void>;
-  linkTcpRTUBuffered(socket: Socket, options: TcpRTUPortOptions, next: Function): void;
+  linkTcpRTUBuffered(
+    socket: Socket,
+    options: TcpRTUPortOptions,
+    next: Function
+  ): void;
   linkTcpRTUBuffered(socket: Socket, options: TcpRTUPortOptions): Promise<void>;
   linkTelnet(socket: Socket, options: TelnetPortOptions, next: Function): void;
   linkTelnet(socket: Socket, options: TelnetPortOptions): Promise<void>;
   connectRTUSocket(socket: Socket, next: Function): void;
   connectRTUSocket(socket: Socket): Promise<void>;
+
+  /**
+   * Set DE/RE transmit control GPIO hook
+   * @param hooks - callbacks to toggle transmit/receive pin
+   */
+  setTransmitControl(hooks: {
+    beforeSend?: () => void | Promise<void>;
+    afterSend?: () => void | Promise<void>;
+  }): void;
 
   // Promise API
   setID(id: number): void;
@@ -56,20 +127,51 @@ export class ModbusRTU {
   getTimeout(): number;
 
   readCoils(dataAddress: number, length: number): Promise<ReadCoilResult>;
-  readDiscreteInputs(dataAddress: number, length: number): Promise<ReadCoilResult>;
-  readHoldingRegisters(dataAddress: number, length: number): Promise<ReadRegisterResult>;
-  readRegistersEnron(dataAddress: number, length: number): Promise<ReadRegisterResult>;
-  readInputRegisters(dataAddress: number, length: number): Promise<ReadRegisterResult>;
+  readDiscreteInputs(
+    dataAddress: number,
+    length: number
+  ): Promise<ReadCoilResult>;
+  readHoldingRegisters(
+    dataAddress: number,
+    length: number
+  ): Promise<ReadRegisterResult>;
+  readRegistersEnron(
+    dataAddress: number,
+    length: number
+  ): Promise<ReadRegisterResult>;
+  readInputRegisters(
+    dataAddress: number,
+    length: number
+  ): Promise<ReadRegisterResult>;
   writeCoil(dataAddress: number, state: boolean): Promise<WriteCoilResult>;
-  writeCoils(dataAddress: number, states: Array<boolean>): Promise<WriteMultipleResult>;
-  writeRegister(dataAddress: number, value: number): Promise<WriteRegisterResult>;
-  writeRegisterEnron(dataAddress: number, value: number): Promise<WriteRegisterResult>;
-  writeRegisters(dataAddress: number, values: Array<number> | Buffer): Promise<WriteMultipleResult>; // 16
-  maskWriteRegister(dataAddress: number, andMask: number, orMask: number): Promise<WriteMaskRegisterResult>;
+  writeCoils(
+    dataAddress: number,
+    states: Array<boolean>
+  ): Promise<WriteMultipleResult>;
+  writeRegister(
+    dataAddress: number,
+    value: number
+  ): Promise<WriteRegisterResult>;
+  writeRegisterEnron(
+    dataAddress: number,
+    value: number
+  ): Promise<WriteRegisterResult>;
+  writeRegisters(
+    dataAddress: number,
+    values: Array<number> | Buffer
+  ): Promise<WriteMultipleResult>; // 16
+  maskWriteRegister(
+    dataAddress: number,
+    andMask: number,
+    orMask: number
+  ): Promise<WriteMaskRegisterResult>;
 
-  on(event: 'close', listener: () => unknown): this;
-  on(event: 'error', listener: (error: unknown) => unknown): this;
-  readDeviceIdentification(deviceIdCode: number, objectId: number): Promise<ReadDeviceIdentificationResult>;
+  on(event: "close", listener: () => unknown): this;
+  on(event: "error", listener: (error: unknown) => unknown): this;
+  readDeviceIdentification(
+    deviceIdCode: number,
+    objectId: number
+  ): Promise<ReadDeviceIdentificationResult>;
   reportServerID(deviceIdCode: number): Promise<ReportServerIDResult>;
 
   isOpen: boolean;
@@ -125,7 +227,7 @@ export interface SerialPortOptions {
   baudRate?: number;
   dataBits?: number;
   stopBits?: number;
-  parity?: 'none' | 'even' | 'mark' | 'odd' | 'space';
+  parity?: "none" | "even" | "mark" | "odd" | "space";
   rtscts?: boolean;
   xon?: boolean;
   xoff?: boolean;
